3aabb8f38991831d0a13699fb06d1b12f8d1a0eb
Michael Naumov <mnaoumov@gmail.com>
2018-04-30 09:52:25 +0300

    Remove fiddler as it is unmaintained now

--- DevSetup.ps1
+++ DevSetup.ps1
@@ -35,7 +35,6 @@ function Main {
         Install-ChocolateyPackage -Name resharper
         Install-ChocolateyPackage -Name GoogleChrome
         Install-ChocolateyPackage -Name Firefox
-        Install-ChocolateyPackage -Name fiddler4
 
         Install-WindowsFeature -Name Web-Server, Web-Mgmt-Console, Web-Scripting-Tools, Web-Asp-Net45
 
